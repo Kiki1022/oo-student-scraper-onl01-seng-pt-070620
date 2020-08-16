529399d561bfef4a33296287386222e3d53cff5d
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-16 23:35:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -6,10 +7,14 @@ class Student
 
   def initialize(student_hash)
     
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+      @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    #binding.pry
   end
 
   def add_student_attributes(attributes_hash)
@@ -17,7 +22,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
