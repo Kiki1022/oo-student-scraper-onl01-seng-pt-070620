27fabd3ff2a7ab10969dbd11720e959fcb695633
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-17 00:27:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,9 +14,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |name, location|
-   binding.pry
- end
+  #   students_array.each do |name, location|
+  # binding.pry
+  #   end
  end
 
   def add_student_attributes(attributes_hash)
